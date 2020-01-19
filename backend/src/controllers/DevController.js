4cf0fc64d3dfaa7c5eda45830c89728435840969
Charles Pereira <charlesti.ptu@gmail.com>
2020-01-19 01:52:53 -0300

    Configuracao latitude e longitude e search

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,29 +1,42 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
+const parseStringArray = require('../utils/parseStringAsArray');
 
 module.exports = {
+  async index(request, response) {
+    const devs = await Dev.find();
+
+    return response.json(devs);
+  },
+
   async store(request, response) {
     const { github_username, techs, latitude, longitude } = request.body;
 
-    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+    let dev = await Dev.findOne({ github_username });
 
-    const { name = login, avatar_url, bio } = apiResponse.data;
+    if (!dev) {
+      const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
 
-    const techArray = techs.split(',').map(tech => tech.trim());
+      const { name = login, avatar_url, bio } = apiResponse.data;
 
-    const location = {
-      type: 'Point',
-      coordinates: [longitude, latitude],
+      const techArray = parseStringArray(techs);
+
+      const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude],
+      }
+
+      dev = await Dev.create({
+        github_username: github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techArray,
+        location
+      });
     }
 
-    const dev = await Dev.create({
-      github_username: github_username,
-      name,
-      avatar_url,
-      bio,
-      techs: techArray,
-      location
-    });
+
 
     return response.json(dev);
   }
---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,29 @@
+const Dev = require('../models/Dev');
+const parseStringArray = require('../utils/parseStringAsArray');
+
+module.exports = {
+  async index(request, response) {
+    //buscar todos os devs em um raio de 10km
+    //filtro de tecnologias
+    const { latitude, longitude, techs } = request.query;
+
+    const techsArray = parseStringArray(techs);
+
+    const devs = await Dev.find({
+      techs: {
+        $in: techsArray,
+      },
+      location: {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [longitude, latitude],
+          },
+          $maxDistance: 10000,
+        },
+      },
+    });
+
+    return response.json({ devs });
+  }
+}
