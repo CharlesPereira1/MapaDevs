eef491336511dc403f71795dfccc62f9ce40d64f
Charles Pereira <charlesti.ptu@gmail.com>
2020-01-20 00:19:23 -0300

    Configurando o mobile - rotas, e Main com nagevacao de mapa

---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Image, View, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+
+export default function Main({ navigation }) {
+  const [currentRegion, setCurrentRegion] = useState(null);
+
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true,
+        });
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04,
+        })
+      }
+    }
+
+    loadInitialPosition();
+  }, [])
+
+  if (!currentRegion) {
+    return null;
+  }
+
+  return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+      <Marker coordinate={{ latitude: -18.9257236, longitude: -48.3192777 }}>
+        <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/54192694?s=460&v=4' }} />
+
+        <Callout onPress={() => {
+          //navegação
+          navigation.navigate('Profile', { github_username: 'charlespereira1' });
+        }}>
+          <View style={styles.callout}>
+            <Text style={styles.devName}>Charles Pereira</Text>
+            <Text style={styles.devBio}>Um apaixonado por desenvolvimento, conhecimentos e criação.</Text>
+            <Text style={styles.devTechs}>ReactJS, React Native, Node.Js</Text>
+          </View>
+        </Callout>
+      </Marker>
+    </MapView>
+  );
+}
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1
+  },
+
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: '#fff'
+  },
+
+  callout: {
+    width: 260,
+  },
+
+  devName: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+
+  devBio: {
+    color: '#666',
+    marginTop: 5,
+  },
+
+  devTechs: {
+    marginTop: 5
+  },
+
+})
